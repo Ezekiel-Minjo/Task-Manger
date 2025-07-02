153939c96662261386123b49f98f8e7f5bb2f5aa
minjo <ezekielminjo4@gmail.com>
2025-07-02 16:19:46 +0300

    Setting up Data structure and variables

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 "use strict";
 console.log("Task Manager loaded! Ready for your JavaScript implementation.");
+// Data structure
+let tasks = []; // Hold all task objects
+let currentFilter = "all"; // Track which filter is active
+let taskIdCounter = 1; // For unique task IDs
+
+// references to DOM elements we'll use frequently
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+const filterBtns = document.querySelectorAll(".filter-btn");
+const clearCompletedBtn = document.getElementById("clearCompleted");
+const emptyState = document.getElementById("emptyState");
+
+// stats elements
+const taskCount = document.getElementById("taskCount");
+const totalTasks = document.getElementById("totalTasks");
+const pendingTasks = document.getElementById("pendingTasks");
+const completedTasks = document.getElementById("completedTasks");
+const CompletionRate = document.getElementById("CompletionRate");
